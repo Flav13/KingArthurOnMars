afdb89d876f4c6a90efb2c5b3c09fd302805ae6d
unknown <mora_umaga@hotmail.com>
2020-12-31 02:05:58 +0000

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    public bool face_left = false;
+
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey("left"))
+        {
+            face_left = true;
+        }
+        else if (Input.GetKey("right"))
+        {
+            face_left = false; 
+        }
+
+        animator.SetFloat("horizontal", movement.x);
+        animator.SetFloat("vertical", movement.y);
+
+        animator.SetFloat("speed", movement.sqrMagnitude);
+        animator.SetBool("face_left", face_left);
+
+
+    }
+
+    void FixedUpdate()
+    {
+        // Movement 
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
